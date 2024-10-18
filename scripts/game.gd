1180ed7fda3f880ec1f98bfc197ccf1d9e6e5ea6
Tim Moss <tmoss726@hotmail.com>
2024-10-17 23:17:54 -0400

    Add lives label

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,17 +7,17 @@ var score = 0
 
 func _ready():
 	hud.set_score_label(score)
+	hud.set_lives_label(lives)
 
 func _on_deathzone_area_entered(area):
 	area.die()
 
 func _on_player_took_damage():
 	lives -= 1
+	hud.set_lives_label(lives)
 	if lives == 0:
 		print("Game Over")
 		player.die()
-	else:
-		print(lives)
 
 # Adds enemies as childs of the Game node instead of the EnemySpawner node
 func _on_enemy_spawner_enemy_spawn(enemy_instance):
